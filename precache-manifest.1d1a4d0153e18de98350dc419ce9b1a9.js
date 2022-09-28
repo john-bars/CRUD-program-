0c558689d72bc36eaf884874c7ce0c379491f545
John Bars <john.barsq@gmail.com>
2022-09-29 07:22:40 +0800

    Updates

---
+++ precache-manifest.1d1a4d0153e18de98350dc419ce9b1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31f9f38b788a414d9cb6",
+    "url": "/crud-program/static/css/main.ec67a3fc.chunk.css"
+  },
+  {
+    "revision": "31f9f38b788a414d9cb6",
+    "url": "/crud-program/static/js/main.31f9f38b.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/crud-program/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/crud-program/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "38b3226a5830183a9a88",
+    "url": "/crud-program/static/js/runtime~main.38b3226a.js"
+  },
+  {
+    "revision": "d46fcf844c327e3c8b7ef813d0e14267",
+    "url": "/crud-program/index.html"
+  }
+];
