7c7d02467cbedc3c3ebaa519b9a5fca78c0d7695
John Bars <john.barsq@gmail.com>
2022-09-30 18:37:44 +0800

    add autoCapitalize function for the input name

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
+function autoCapitalize () {
+	let firstName = document.getElementById("firstName")
+	firstName.value=firstName.value.toUpperCase();
+
+	let lastName = document.getElementById("lastName")
+	lastName.value=lastName.value.toUpperCase();
+}
 $(document).ready(function () {
 
 	$("#btnAdd").click(function () {
-		let firstName = $("#firstName").val().trim();
-		let lastName = $("#lastName").val().trim();
-		let number = $("#number").val().trim();
-		let address = $("#address").val().trim();
-		let email = $("#email").val().trim();
+		// let firstName = $("#firstName").val().trim();
+		// let lastName = $("#lastName").val().trim();
+		// let number = $("#number").val().trim();
+		// let address = $("#address").val().trim();
+		// let email = $("#email").val().trim();
+
+		let firstName = $("#firstName").val();
+		let lastName = $("#lastName").val();
+		let number = $("#number").val();
+		let address = $("#address").val();
+		let email = $("#email").val();
 
 		if (firstName != "" && lastName != "" && number != "" && address != "" && email != "") {
 			let serialNumber = $("#tblData tbody").children().length + 1;
 
 			// creating dynamic html string
-			let dynamicTr = "<tr><td>" + serialNumber + "</td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + number + "</td><td>" + address + "</td><td>" + email + "</td> <td> <button class='btn btn__edit'> Edit </button><button class='btn btn-sm btn__delete'> Delete </button> </td><tr>";
+			let dynamicTr = "<tr><td>" + serialNumber + "</td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + number + "</td><td>" + address + "</td><td>" + email + "</td> <td> <button class='btn btn__edit'> Edit </button><button class='btn btn__delete'> Delete </button> </td><tr>";
 			$("#tblData tbody").append(dynamicTr); //appending dynamic string to the table
+
+			// removing the last typed data on the input tag 
 			$("#firstName").val("");
 			$("#lastName").val("");
 			$("#number").val("");
 			$("#address").val("");
 			$("#email").val("");
-			$(".btn-sm").click(function () {
+			$(".btn__delete").click(function () {
 				$(this).parent().parent().remove();
 			});
 		} 
